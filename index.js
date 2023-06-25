bf2cef5a9f6c7800c32e70282b863da6ce6b0155
sameermd16 <sameerthemc@gmail.com>
2023-06-25 10:37:22 +0530

    replies

--- index.js
+++ index.js
@@ -7,21 +7,19 @@
 import {tweetsData} from './data.js'
 
 
-const tweetBtn = document.getElementById("tweet-btn")
 const tweetInput = document.getElementById("tweet-input")
 
-
-
-tweetBtn.addEventListener('click', () => {
-    console.log(tweetInput.value)
-})
 
 document.addEventListener('click', (e) => {
     if(e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like)
     } else if (e.target.dataset.retweet) {
         handleRetweetsClick(e.target.dataset.retweet)
-        console.log(e.target.dataset.retweet)
+    } else if (e.target.dataset.reply) {
+        // console.log(e.target.dataset.reply)
+        handleReplyClick(e.target.dataset.reply)
+    } else if (e.target.id === 'tweet-btn') {
+        handleTweetBtnClick() 
     }
 })
 
@@ -46,16 +44,34 @@ function handleRetweetsClick(tweetId) {
     const targetTweetObj = tweetsData.filter((object) => {
         return object.uuid === tweetId
     })[0]
-    if(targetTweetObj.isRetweeted) {
-        targetTweetObj.retweets--
-    }else {
+    if(!targetTweetObj.isRetweeted) {
         targetTweetObj.retweets++
+        targetTweetObj.isRetweeted = true
+    }else {
+        targetTweetObj.retweets--
+        targetTweetObj.isRetweeted = false
     }
-    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    // targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
     // console.log(targetTweetObj)
     render()
 }
 
+function handleReplyClick(tweetId) {
+    const targetTweetObj = tweetsData.filter((object) => {
+        return object.uuid === tweetId
+    })[0]
+    // console.log(targetTweetObj)
+    document.getElementById(`replies-${tweetId}`).classList.toggle('hidden')
+    // if (targetTweetObj.replies.length > 0) {
+    //     //show the replies div
+    // } else {
+    //     //keep hiding the replies div
+    // }
+}
+
+function handleTweetBtnClick() {
+    console.log(tweetInput.value)
+}
 
 function getFeedHtml() {
     let feedHtml = ''
@@ -83,7 +99,35 @@ function getFeedHtml() {
     //         </div>         
     //     `
     // }
+    
     tweetsData.forEach((eachObject) => {
+        let heartClass = ''
+        let retweetClass = ''
+
+        if (eachObject.isLiked) {
+            heartClass = 'liked'
+        } else if (eachObject.isRetweeted) {
+            retweetClass = 'retweeted'
+        }
+        let repliesHtml = ''
+        let repliesClass = ''
+        if(eachObject.replies.length > 0) {
+            eachObject.replies.forEach((object) => {
+                // console.log(object)
+                repliesHtml += `
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="${object.profilePic}" class="profile-pic">
+                                <div>
+                                    <p class="handle">${object.handle}</p>
+                                    <p class="tweet-text">${object.tweetText}</p>
+                                </div>
+                        </div>
+                    </div>
+                `
+            })
+        }
+        
         feedHtml += `
              <div class="tweet">
                  <div class="tweet-inner">
@@ -97,15 +141,18 @@ function getFeedHtml() {
                                  ${eachObject.replies.length} 
                              </span>
                              <span class="tweet-detail">
-                                <i class="fa-solid fa-heart" data-like="${eachObject.uuid}"></i>
+                                <i class="fa-solid fa-heart ${heartClass}" data-like="${eachObject.uuid}"></i>
                                  ${eachObject.likes}
                              </span>
                              <span class="tweet-detail">
-                                <i class="fa-solid fa-retweet" data-retweet="${eachObject.uuid}"></i>
+                                <i class="fa-solid fa-retweet ${retweetClass}" data-retweet="${eachObject.uuid}"></i>
                                  ${eachObject.retweets}
                              </span>
                          </div>   
                      </div>            
+                 </div>
+                 <div id="replies-${eachObject.uuid}" class="${repliesClass}">
+                    ${repliesHtml}
                  </div>
              </div>         
          `
